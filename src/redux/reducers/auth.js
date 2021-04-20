c692baa4391ce64a75586d40ece54ea7a554dd6c
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-04-20 10:17:10 +0700

    fix: reset user reducers

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,22 +1,22 @@
 import { SET_USER, SET_ERROR, SET_LOADING } from "../actions/actionTypes";
 
 const initialState = {
-  user: {
-    avatar: "avatars/avatar-18-1617841017228.png",
-    id: 18,
-    name: "Burhan si Instructors",
-    username: "burhan",
-    role_id: 1,
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxOCwibmFtZSI6IkJ1cmhhbiBzaSBJbnN0cnVjdG9ycyIsInJvbGVfaWQiOjEsImlhdCI6MTYxODg4MzIyNSwiZXhwIjoxNjE4OTY5NjI1LCJpc3MiOiJCRUxBSkFSU0lQIn0.0hBfhMKe8gm-2OO6pcLdt0a9om65IXgEYs_rY040Ukw",
-  },
-  isLoggedIn: true,
-  isError: false,
-  isLoading: false,
-  // user: {},
-  // isLoggedIn: false,
+  // user: {
+  //   avatar: "avatars/avatar-18-1617841017228.png",
+  //   id: 18,
+  //   name: "Burhan si Instructors",
+  //   username: "burhan",
+  //   role_id: 1,
+  //   token:
+  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxOCwibmFtZSI6IkJ1cmhhbiBzaSBJbnN0cnVjdG9ycyIsInJvbGVfaWQiOjEsImlhdCI6MTYxODg4MzIyNSwiZXhwIjoxNjE4OTY5NjI1LCJpc3MiOiJCRUxBSkFSU0lQIn0.0hBfhMKe8gm-2OO6pcLdt0a9om65IXgEYs_rY040Ukw",
+  // },
+  // isLoggedIn: true,
   // isError: false,
   // isLoading: false,
+  user: {},
+  isLoggedIn: false,
+  isError: false,
+  isLoading: false,
   error: {},
 };
 
