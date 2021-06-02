59a6294076c2bd2d74d07292e6c2e4178c1120b9
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-06-02 16:32:20 +0700

    refactor: remove unused variable

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -6,18 +6,21 @@ const BASE_URL = process.env.REACT_APP_API;
 
 export function loginHandler(username, password) {
   return (dispatch) => {
-    dispatch({
-      type: SET_LOADING,
-    });
+    // dispatch({
+    //   type: SET_LOADING,
+    // });
+    console.log(`${BASE_URL}/v1/auth/login`);
     axios
       .post(`${BASE_URL}/v1/auth/login`, {
         username,
         password,
       })
       .then((res) => {
+        console.log(res);
         dispatch({ type: SET_USER, payload: res.data.data });
       })
       .catch((err) => {
+        console.log(err);
         dispatch({
           type: SET_ERROR,
           payload: err,
